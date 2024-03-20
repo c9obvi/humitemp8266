b78f739da4a694ff0af0f824096dd4f8bb3ee332
0xBerto.Ξ <31329416+c9obvi@users.noreply.github.com>
2024-03-19 20:24:05 -0700

    Updated humitemp.ino
    CSS, better notifications, emojis and overall experience as well as cleaner code

--- humitemp.ino
+++ humitemp.ino
@@ -9,8 +9,8 @@
 #define DHTTYPE DHT11     // DHT 11
 DHT dht(DHTPIN, DHTTYPE);
 
-const char* ssid = "wifi_name";        // Your network SSID (name)
-const char* password = "PW";    // Your network password
+const char* ssid = "*****";        // Your network SSID (name)
+const char* password = "*****";    // Your network password
 
 ESP8266WebServer server(80);
 
@@ -30,10 +30,19 @@ void handleData() {
 }
 
 // Notification flags
-bool tempAbove25CNotified = false;
 bool humidityBelow30Notified = false;
 bool humidityAbove75Notified = false;
+
+bool tempAbove70FNotified = false;
 bool tempAbove75FNotified = false;
+bool tempAbove80FNotified = false;
+bool tempAbove85FNotified = false;
+bool tempAbove90FNotified = false;
+
+
+bool tempBelow70FNotified = false;
+bool tempBelow65FNotified = false;
+bool tempBelow55FNotified = false;
 bool tempBelow45FNotified = false;
 
 void sendNotification(String message) {
@@ -42,7 +51,7 @@ void sendNotification(String message) {
     client->setInsecure(); // Bypass SSL certificate verification
 
     HTTPClient http;
-    http.begin(*client, "https://ntfy.sh/humitemp"); // Use your Ntfy URL
+    http.begin(*client, "https://ntfy.sh/*****"); // Use your Ntfy URL
     
     Serial.println("Sending notification: " + message);
     
@@ -76,18 +85,38 @@ void setup() {
   Serial.println(WiFi.localIP());
 
   delay(1000); // Give a moment for the connection to stabilize.
-  sendNotification("humitemp is online!"); // Attempt to notify.
+  sendNotification("humitemp is online!🚀"); // Attempt to notify.
+
+// Additionally, send a notification with the IP address
+  String ipNotification = "Device IP: " + WiFi.localIP().toString();
+  sendNotification(ipNotification);
+
+// After sending IP address notification
+float initialHumidity = dht.readHumidity();
+float initialTemperatureC = dht.readTemperature(); // Read temperature in Celsius
+float initialTemperatureF = initialTemperatureC * 9.0 / 5.0 + 32.0; // Convert to Fahrenheit
+
+// Prepare the message with initial readings in Fahrenheit
+String initialReadings = "Initial readings - Temperature: " + String(initialTemperatureF, 2) + " °F🌡️, Humidity: " + String(initialHumidity, 2) + "%💧";
+sendNotification(initialReadings);
+
 
 server.on("/", HTTP_GET, []() {
   String html = 
-    "<!DOCTYPE html>"
-    "<html>"
-    "<head>"
-    "<title>ESP8266 DHT11 Server</title>"
-    "<meta http-equiv='refresh' content='10'>" // This line causes the page to refresh every 10 seconds
-    "</head>"
-    "<body>"
-    "<h1>Temperature and Humidity</h1>";
+  "<!DOCTYPE html>"
+  "<html>"
+  "<head>"
+  "<title>ESP8266 DHT11 Server</title>"
+  "<meta name='viewport' content='width=device-width, initial-scale=1.0'>" // Make it responsive
+  "<style>"
+  "body { display: flex; justify-content: center; align-items: center; flex-direction: column; height: 100vh; margin: 0; }"
+  ".container { text-align: center; max-width: 600px; width: 100%; padding: 20px; }"
+  "</style>"
+  "<meta http-equiv='refresh' content='10'>" // This line causes the page to refresh every 10 seconds
+  "</head>"
+  "<body>"
+  "<div class='container'>"
+  "<h1>Temperature and Humidity &#x1F3E0;</h1>";
 
   // Attempt to read from sensor
   float h = dht.readHumidity();
@@ -98,8 +127,8 @@ server.on("/", HTTP_GET, []() {
   if (isnan(h) || isnan(t)) {
     html += "<p>Failed to read from DHT sensor!</p>";
   } else {
-    html += "<p>Temperature: " + String(t) + " °C (" + String(f) + " °F)</p>"
-            "<p>Humidity: " + String(h) + " %</p>";
+    html += "<p>Temperature: " + String(f) + " °F)&#x1F321;</p>"
+            "<p>Humidity: " + String(h) + " %&#x1F4A7;</p>";
   }
 
   html += 
@@ -113,6 +142,21 @@ server.on("/", HTTP_GET, []() {
 
   server.begin();
 }
+
+void checkTemperatureAndNotify(float currentTemp, float threshold, bool &notifiedFlag, String messageAbove, String messageBelow) {
+  if (currentTemp > threshold && !notifiedFlag) {
+    if (messageAbove != "") {
+      sendNotification(messageAbove + ": " + String(currentTemp));
+    }
+    notifiedFlag = true;
+  } else if (currentTemp <= threshold && notifiedFlag) {
+    if (messageBelow != "") {
+      sendNotification(messageBelow + ": " + String(currentTemp));
+    }
+    notifiedFlag = false;
+  }
+}
+
 
 void loop() {
 
@@ -128,43 +172,34 @@ void loop() {
   float temperatureF = dht.readTemperature(true);
   float humidity = dht.readHumidity();
 
-  // Temperature above 25°C
-  if (!isnan(temperatureC) && temperatureC > 25.0 && !tempAbove25CNotified) {
-    sendNotification("Temperature is above 25 °C: " + String(temperatureC));
-    tempAbove25CNotified = true;
-  } else if (!isnan(temperatureC) && temperatureC <= 25.0 && tempAbove25CNotified) {
-    tempAbove25CNotified = false;
-  }
+ // Humidity checks
+  if (!isnan(humidity)) {
+    if (humidity < 30.0 && !humidityBelow30Notified) {
+      sendNotification("Humidity is below 30%: " + String(humidity));
+      humidityBelow30Notified = true;
+    } else if (humidity >= 30.0 && humidityBelow30Notified) {
+      humidityBelow30Notified = false;
+    }
 
-  // Humidity below 30%
-  if (!isnan(humidity) && humidity < 30.0 && !humidityBelow30Notified) {
-    sendNotification("Humidity is below 30%: " + String(humidity));
-    humidityBelow30Notified = true;
-  } else if (!isnan(humidity) && humidity >= 30.0 && humidityBelow30Notified) {
-    humidityBelow30Notified = false;
+    if (humidity > 75.0 && !humidityAbove75Notified) {
+      sendNotification("Humidity is above 75%: " + String(humidity));
+      humidityAbove75Notified = true;
+    } else if (humidity <= 75.0 && humidityAbove75Notified) {
+      humidityAbove75Notified = false;
+    }
   }
+  // Temperature checks
+  if (!isnan(temperatureF)) {
+    checkTemperatureAndNotify(temperatureF, 70.0, tempAbove70FNotified, "Temperature is above 70 °F🌡️");
+    checkTemperatureAndNotify(temperatureF, 75.0, tempAbove75FNotified, "Temperature is above 75 °F🌡️", "");
+    checkTemperatureAndNotify(temperatureF, 80.0, tempAbove80FNotified, "Temperature is above 80 °F🌡️", "");
+    checkTemperatureAndNotify(temperatureF, 85.0, tempAbove85FNotified, "⚠️ Caution, temperature is above 85 °F🌡️", "");
+    checkTemperatureAndNotify(temperatureF, 90.0, tempAbove90FNotified, "⚠️ 🔥 WARNING ⚠️ temperature is above 90 °F🌡️🔥", "");
 
-  // Humidity above 75%
-  if (!isnan(humidity) && humidity > 75.0 && !humidityAbove75Notified) {
-    sendNotification("Humidity is above 75%: " + String(humidity));
-    humidityAbove75Notified = true;
-  } else if (!isnan(humidity) && humidity <= 75.0 && humidityAbove75Notified) {
-    humidityAbove75Notified = false;
-  }
-
-  // Temperature above 75°F
-  if (!isnan(temperatureF) && temperatureF > 75.0 && !tempAbove75FNotified) {
-    sendNotification("Temperature is above 75 °F: " + String(temperatureF));
-    tempAbove75FNotified = true;
-  } else if (!isnan(temperatureF) && temperatureF <= 75.0 && tempAbove75FNotified) {
-    tempAbove75FNotified = false;
-  }
-
-  // Temperature below 45°F
-  if (!isnan(temperatureF) && temperatureF < 45.0 && !tempBelow45FNotified) {
-    sendNotification("Temperature is below 45 °F: " + String(temperatureF));
-    tempBelow45FNotified = true;
-  } else if (!isnan(temperatureF) && temperatureF >= 45.0 && tempBelow45FNotified) {
-    tempBelow45FNotified = false;
+    checkTemperatureAndNotify(temperatureF, 70.0, tempBelow70FNotified, "", "Temperature is below 70 °F🌡️");
+    checkTemperatureAndNotify(temperatureF, 65.0, tempBelow65FNotified, "", "Temperature is below 65 °F🌡️");
+    checkTemperatureAndNotify(temperatureF, 55.0, tempBelow55FNotified, "", "🥶 temperature is below 55 °F🌡️");
+    checkTemperatureAndNotify(temperatureF, 45.0, tempBelow45FNotified, "", "🥶 WARNING, temperature is below 45 °F❄️🌡️");
   }
 }
+
